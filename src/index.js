841b19f3f9cb70014a8a64b08f83dad15f80be50
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-15 14:19:50 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  newTaskForm.addEventListener("submit", addNewTask);
+
 });
+
+
+
+const addNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendTask(newTask);
+};
+
+  const appendTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
